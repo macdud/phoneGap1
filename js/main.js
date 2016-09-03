ac6e58479693bb327e66845831834e63690f15db
Maciej\Maciej <mdudzi90@gmail.com>
2016-09-03 20:35:27 +0100

    update

--- js/main.js
+++ js/main.js
@@ -12,11 +12,47 @@ var app = {
             }
         });
     },
-
+	
+       
+	 // This optional function html-encodes messages for display in the page.
+      
     initialize: function() {
         this.store = new MemoryStore();
+		var   htmlEncode = function(value) {
+            var encodedValue = $('<div />').text(value).html();
+            return encodedValue;
+        }
+		
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+		  // Reference the auto-generated proxy for the hub.
+            $.connection.hub.url = 'http://macdud-001-site1.itempurl.com/signalr/hubs';
+            var chat = $.connection.hubClass;
+			var displayName = 'Maciej'; //prompt('Enter your name:', '')
+            // Create a function that the hub can call back to display messages.
+            chat.client.addNewMessageToPage = function (name, message) {
+                // Add the message to the page.
+                $('#discussion').append('<li><strong>' + htmlEncode(name)
+                    + '</strong>: ' + htmlEncode(message) + '</li>');
+            };
+            // Get the user name and store it to prepend to messages.
+            $('#displayname').val(displayName); 
+            // Set initial focus to message input box.
+            $('#message').focus();
+
+            chat.client.hello = function (message) {
+                $('#discussion').append('<li><strong>' + htmlEncode(message) + '</li>');
+            };
+
+            // Start the connection.
+            $.connection.hub.start().done(function () {
+                $('#sendmessage').click(function () {
+                    // Call the Send method on the hub.
+                    chat.server.send(displayName, $('#message').val());
+                    // Clear text box and reset focus for next comment.
+                    $('#message').val('').focus();
+                });
+            });
+        }
 
 };
 

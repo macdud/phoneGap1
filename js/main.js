2131b5e9129f3930aaf063783374a9b43ed68d9c
Maciej\Maciej <mdudzi90@gmail.com>
2016-09-10 21:32:52 +0100

    sadsadsadsaa

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var map = "";
 var iconFeatures = [];
 
+var vectorSource = new ol.source.Vector({
+	  features: iconFeatures
+});
+	
 function loadStuff()
 	{
 			/*http://macdud-001-site1.itempurl.com/api/*/
@@ -37,14 +41,6 @@ function loadMap()
 	 var zoom = 16;
 	 var scale = 0.1;
 	 
-	 
-	addIcon(lon, lat, 0.1, 'logo.png');
-	
-	addIcon(lon + 0.005, lat, 0.05, 'fire.png');
-	
-	var vectorSource = new ol.source.Vector({
-	  features: iconFeatures
-	});
 
 	var vectorLayer = new ol.layer.Vector({
 	  source: vectorSource
@@ -69,9 +65,14 @@ function loadMap()
       });
 	
 	  centerMap(lon, lat, zoom);
-	  
-	  //addPopup();
-	  
+	
+	var iconData = {title:'Maciej Dudzinski', text: 'Good lad'};
+	addIcon(iconData,lon, lat, 0.1, 'logo.png');
+	
+	
+	  addPopup();
+	  	
+		
       document.getElementById('zoom-out').onclick = function() {
         var view = map.getView();
         var zoom = view.getZoom();
@@ -92,12 +93,13 @@ function centerMap(lon, lat, zoom)
 		map.getView().setZoom(zoom);
 }
 
-function addIcon(lon, lat, scale, type)
+function addIcon(data,lon, lat, scale, type)
 {
 	var iconFeature = new ol.Feature({
 		geometry: new ol.geom.Point(ol.proj.transform([lon,lat], 'EPSG:4326',     
               'EPSG:3857')),
-		name: 'Null Island',
+		name: 'Feature',
+		oData: data,
 		population: 4000,
 		rainfall: 500
 	});
@@ -116,49 +118,70 @@ function addIcon(lon, lat, scale, type)
 	iconFeature.setStyle(iconStyle);
 	
 	iconFeatures.push(iconFeature);
+	
+	vectorSource.clear();
+	vectorSource.addFeatures(iconFeatures);
 
 }
 
 function addPopup()
 {
-	var element = document.getElementById('popup');
-
-	var popup = new ol.Overlay({
-	  element: element,
-	  positioning: 'bottom-center',
-	  stopEvent: false
-	});
-	map.addOverlay(popup);
-
 	// display popup on click
 	map.on('click', function(evt) {
-	  var feature = map.forEachFeatureAtPixel(evt.pixel,
-		  function(feature, layer) {
-			return feature;
-		  });
-	  if (feature) {
-		popup.setPosition(evt.coordinate);
-		$(element).popover({
-		  'placement': 'top',
-		  'html': true,
-		  'content': feature.get('name')
-		});
-		$(element).popover('show');
-	  } else {
-		$(element).popover('destroy');
-	  }
+		
+	   var feature = map.forEachFeatureAtPixel(evt.pixel,
+		   function(feature, layer) {
+			 return feature;
+		   });
+	   if (feature) {
+		 var data = feature.get('oData');
+		 showModal(data);
+		 
+	   } else {
+			var coord = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
+			var lon = coord[0];
+			var lat = coord[1];
+			console.log(lon + " " + lat );
+			var data = {title : 'Fire !!!', text : 'some text'};
+			addIcon(data,lon, lat + 0.0001, 0.05, 'fire.png');
+	   }
 	});
 
-	// change mouse cursor when over marker
-	map.on('pointermove', function(e) {
-	  if (e.dragging) {
-		$(element).popover('destroy');
-		return;
-	  }
-	  var pixel = map.getEventPixel(e.originalEvent);
-	  var hit = map.hasFeatureAtPixel(pixel);
-	  map.getTarget().style.cursor = hit ? 'pointer' : '';
-	});
+	 // change mouse cursor when over marker
+	 map.on('pointermove', function(e) {
+	   // if (e.dragging) {
+		 // $(element).popover('destroy');
+		 // return;
+	   // }
+	   var pixel = map.getEventPixel(e.originalEvent);
+	   var hit = map.hasFeatureAtPixel(pixel);
+	   //map.getTarget().style.cursor = hit ? 'pointer' : '';
+	   $('.map').css('cursor' , hit == true ? 'pointer' : 'auto');
+	 });
+}
+
+function showModal(data)
+{
+	var modal = document.getElementById('myModal');
+	$('.modal-title').html(data.title);
+	$('.modal-text').html(data.text);
+	modal.style.display = "block";
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
+	
 }
 		
 		
